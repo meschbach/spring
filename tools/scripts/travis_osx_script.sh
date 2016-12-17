250f65047d3a089756ee1943c10ed7ef512508f3
Mark Eschbach <meschbach@gmail.com>
2016-12-17 11:08:40 -0800

    [ci/travis] Overriding Clang to use GCC compatiable driver

--- tools/scripts/travis_osx_script.sh
+++ tools/scripts/travis_osx_script.sh
@@ -3,7 +3,7 @@
 which cmake
 cmake --version
 
-cmake . || (
+make -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DAPPLE=1 -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_SYSTEM_PROCESSOR=x86_64 .|| (
 	e=$?
 	echo "===== CMakeOutput.log ===="
 	cat ./CMakeFiles/CMakeOutput.log
